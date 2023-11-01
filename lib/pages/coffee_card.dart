a93f4f1a03d87547a7f2f19afcd70b9856562c42
casey <caseyochieng46@gmail.com>
2023-11-01 19:38:21 +0300

    added a new card

--- lib/pages/coffee_card.dart
+++ lib/pages/coffee_card.dart
@@ -8,10 +8,43 @@ class CoffeeCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0, top: 25.0, bottom: 8.0),
       child: Container(
+        padding: const EdgeInsets.all(12.0),
         width: 200,
         decoration: const BoxDecoration(
           color: Colors.black54,
           borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Coffee Image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/latte.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            // Coffee Name
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Latte',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  //Text Description.
+                  Text(
+                    'With Almond Milk',
+                    style: TextStyle(color: Colors.grey[700]),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
